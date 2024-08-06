342f17b11ce256769364a768d7e63cc1381063e9
riham esmail <rihamesmail80@gmail.com>
2024-08-06 18:11:37 +0300

    initial commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,285 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta content="IE=edge" http-equiv="X-UA-Compatible">
+    <meta content="width=device-width,initial-scale=1" name="viewport">
+    <meta content="description" name="description">
+    <meta name="google" content="notranslate" />
+    <meta content="Mashup templates have been developped by Orson.io team" name="author">
+
+    <!-- Disable tap highlight on IE -->
+    <meta name="msapplication-tap-highlight" content="no">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="{{asset('Front/assets/apple-icon-180x180.png')}}">
+    <link href="{{asset('Front/assets/favicon.ico')}}" rel="icon">
+
+    <title>CV page</title>
+
+    <link href="{{asset('Front/main.d8e0d294.css')}}" rel="stylesheet"></head>
+
+<body class="">
+
+<!-- Add your content of header -->
+<div class="background-color-layer" style="background-image: url('Front/assets/images/personal-img.jpg')"
+></div>
+<main class="content-wrapper">
+    <header class="white-text-container section-container">
+        <div class="text-center">
+            <h1>I am Riham Esmail</h1>
+            <p>Laravel Developer</p>
+            <p>
+                <a class="fa-icon fa-icon-2x" href="https://facebook.com/" title="">
+                    <i class="fa fa-facebook"></i>
+                </a>
+                <a class="fa-icon fa-icon-2x" href="https://twitter.com/" title="">
+                    <i class="fa fa-twitter"></i>
+                </a>
+                <a class="fa-icon fa-icon-2x" href="https://dribbble.com/" title="">
+                    <i class="fa fa-dribbble"></i>
+                </a>
+                <a class="fa-icon fa-icon-2x" href="https://www.linkedin.com/" title="">
+                    <i class="fa fa-linkedin"></i>
+                </a>
+                <a class="fa-icon fa-icon-2x" href="https://vimeo.com/" title="">
+                    <i class="fa fa-vimeo"></i>
+                </a>
+            </p>
+        </div>
+    </header>
+
+
+
+    <!-- Add your site or app content here -->
+
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+
+                <div class="card">
+                    <div class="card-block">
+                        <h2>About me</h2>
+                        <div class="row">
+                            <div class="col-md-4">
+                                <p><img src="{{asset('Front/assets/images/personal-img.jpg')}}" class="img-responsive" alt=""></p>
+                            </div>
+                            <div class="col-md-8">
+                                <p>Highly motivated and passionate information engineer seeking an opportunity to contribute to a dynamic team and gain practical experience in developing robust web applications using laravel framework.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="card-block">
+                        <h2>Projects</h2>
+                        <div class="row">
+                            <div class="col-md-4">
+                                <img src="{{asset(asset('Front/assets/images/sanctum.png'))}}" class="img-responsive" alt="">
+                                <h3 class="h5">Simple Auth System</h3>
+                                <p>June 2024</p>
+                            </div>
+                            <div class="col-md-4">
+                                <img src="{{asset('Front/assets/images/RBAC.png')}}" class="img-responsive" alt="">
+                                <h3 class="h5">RBAC subsystem</h3>
+                                <p>June 2024</p>
+                            </div>
+                            <div class="col-md-4">
+                                <img src="{{asset('Front/assets/images/stripe-gateway.jpg')}}" class="img-responsive" alt="">
+                                <h3 class="h5">Online Payment</h3>
+                                <p>June 2024</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="card-block">
+                        <h2>Experience</h2>
+                        <div class="work-experience">
+                            <small class="date">Mar 2024 - May 2024</small>
+                            <h3 class="h5 date-title">Laravel Development Intern </h3>
+                            <h5 class="h5 date-title">Mad-Solution, Online </h5>
+                            <p>  Worked on implementing features and functionalities, such as user authentication, authorization, Permissions and Roles, and payment processing, using Laravel's built-in tools and libraries.</p>
+                            <p>  Managed database operations including schema design, migrations, and seeding using Laravel's Eloquent ORM.</p>
+                            <p>  Adhered to clean code principles to ensure readability, maintainability, and scalability of the codebase.</p>
+                            <p>  Designed, developed, and maintained RESTful APIs using Laravel to support various web and mobile applications.</p>
+                        </div>
+                        <div class="work-experience">
+                            <small class="date">Aug 2022 - Feb 2024</small>
+                            <h3 class="h5 date-title">IT Help Desk </h3>
+                            <h5 class="h5 date-title">Safita Council City, Tartous </h5>
+                            <p>Diagnosed and resolved software-related issues, ensuring minimal downtime and maintaining productivity for end users.</p>
+                            <p>Standardized and streamlined the process for formatting and reinstalling operating systems.</p>
+                        </div>
+                        <div class="work-experience">
+                            <small class="date">Mar 2020 - Feb 2021</small>
+                            <h3 class="h5 date-title">Technology Content Writer </h3>
+                            <h5 class="h5 date-title">Freelancer </h5>
+                            <p>Conducted in-depth research on emerging technologies, software, and industry trends to write informative and engaging articles</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-block">
+                        <h2>Education</h2>
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="education-experience">
+                                    <small class="date">Oct 2013 - Jul 2018</small>
+                                    <h3 class="h5 date-title">Informatics Engineering</h3>
+                                    <p>Tishreen University</p>
+                                </div>
+
+                            </div>
+                            <div class="col-md-4">
+                                <div class="education-experience">
+                                    <small class="date">2023</small>
+                                    <h3 class="h5 date-title">laravel basics course</h3>
+                                    <p>Glory Institute</p>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="education-experience">
+                                    <small class="date">2020</small>
+                                    <h3 class="h5 date-title">Computer Maintenance Course</h3>
+                                    <p>AL noor Institute</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="card-block">
+                        <h2>Language</h2>
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="language-experience">
+                                    <h3 class="h5">Arabic  <small>Native speaker</small></h3>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="language-experience">
+                                    <h3 class="h5">English  <small>very good </small></h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="card-block">
+                        <h2>HONORS & AWARDS</h2>
+                        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+
+                            <!-- Wrapper for slides -->
+                            <div class="carousel-inner" role="listbox">
+                                <div class="item active">
+                                    <img src="{{asset('Front/assets/images/best-trainee.jpg')}}" class="img-responsive" alt="...">
+                                </div>
+                                <div class="item">
+                                    <img src="{{asset('Front/assets/images/best-trainee.jpg')}}" class="img-responsive" alt="...">
+                                </div>
+                            </div>
+
+                            <!-- Indicators -->
+                            <ol class="carousel-indicators">
+                                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                            </ol>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="card-block">
+                        <h2>Social Network</h2>
+                        <div class="row">
+                            <div class="col-md-3">
+                                <p class="social-buttons"><a href="https://www.linkedin.com/in/riham-esmail-0b3b10194/" title=""><span class="social-round-icon fa-icon"><i class="fa fa-linkedin"></i></span>@Riham Esmail</a></p>
+                            </div>
+                            <div class="col-md-3">
+                                <p class="social-buttons"><a  href="https://www.facebook.com/profile.php?id=100007965588108&sk=map" title=""><span class="social-round-icon fa-icon"><i class="fa fa-facebook"></i></span>@Riham Esmail</a></p>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="card-block">
+                        <h2>Contact</h2>
+                        <form action="" class="reveal-content">
+                            <div class="form-group">
+                                <input type="email" class="form-control" id="email" placeholder="Email">
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" id="subject" placeholder="Subject">
+                            </div>
+                            <div class="form-group">
+                                <textarea class="form-control" rows="5" placeholder="Enter your message"></textarea>
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" class=" btn btn-primary">Send message</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+</main>
+<footer class="footer-container white-text-container text-center">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <p><img src="{{asset('Front/assets/images/mashup-icon.svg')}}" alt=""></p>
+                <p>
+                    <a class="fa-icon fa-icon-2x" href="https://facebook.com/" title="">
+                        <i class="fa fa-facebook"></i>
+                    </a>
+                    <a class="fa-icon fa-icon-2x" href="https://twitter.com/" title="">
+                        <i class="fa fa-twitter"></i>
+                    </a>
+                    <a class="fa-icon fa-icon-2x" href="https://dribbble.com/" title="">
+                        <i class="fa fa-dribbble"></i>
+                    </a>
+                    <a class="fa-icon fa-icon-2x" href="https://www.linkedin.com/" title="">
+                        <i class="fa fa-linkedin"></i>
+                    </a>
+                    <a class="fa-icon fa-icon-2x" href="https://vimeo.com/" title="">
+                        <i class="fa fa-vimeo"></i>
+                    </a>
+                </p>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<script>
+    document.addEventListener("DOMContentLoaded", function (event) {
+        scrollRevelation('.card');
+    });
+</script>
+<!-- Google Analytics: change UA-XXXXX-X to be your site's ID
+
+<script>
+  (function (i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+      (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date(); a = s.createElement(o),
+      m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+  })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+  ga('create', 'UA-XXXXX-X', 'auto');
+  ga('send', 'pageview');
+</script>
+
+--><script type="text/javascript" src="{{asset('Front/main.bc58148c.js')}}"></script></body>
+
+</html>
